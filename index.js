6b008614d8a83b11faa8fa568141e658ce10c30d
CodyIsTheSenate <mathieuman78@gmail.com>
2022-11-15 10:29:55 +0100

    Add event ready

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
 const config = require("./config.json");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
+
+fs.readdir(__dirname + "/event/", (err, files) => {
+  files.forEach((file) => {
+    if (fs.existsSync(__dirname + "/event/" + file)) require(__dirname + "/event/" + file).run(client);
+  });
 });
 
 client.login(config.token);
