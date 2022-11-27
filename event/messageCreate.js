2fdc00beab801aa97c1b8b6bb15fb0f6c30be63a
CodyIsTheSenate <mathieuman78@gmail.com>
2022-11-27 14:53:44 +0100

    Save welcome channel

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -2,6 +2,7 @@ module.exports.run = (client) => {
   client.on("messageCreate", (message) => {
     switch (message.content) {
       case "!DVFL welcome channel":
+        if (message.deletable) message.delete();
         const guild = message.guild;
         require("../textChannel/welcome/index").run(guild);
         break;
--- textChannel/welcome/index.js
+++ textChannel/welcome/index.js
@@ -1,12 +1,39 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ChannelType } = require("discord.js");
+const fs = require("fs");
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Events,
+  ChannelType,
+} = require("discord.js");
+const botDataPath = __dirname + "/../../data/bot-data.json";
 
 module.exports.run = async (guild) => {
+  let data = JSON.parse(fs.readFileSync(botDataPath));
+  const welcomeChannelId = data.guilds[guild.id].welcomeChannelId;
+  if (welcomeChannelId) {
+    const welcomeChannel = await guild.channels
+      .fetch(welcomeChannelId)
+      .catch((err) => {
+        if (err.code != 10003) throw new Error(err);
+      });
+    if (welcomeChannel) return;
+  }
+
   const channel = await guild.channels.create({
     name: "bienvenue",
     type: ChannelType.GuildText,
   });
 
-  const row = new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("register").setLabel("S'enregistrer").setStyle(ButtonStyle.Primary));
+  data.guilds[guild.id].welcomeChannelId = channel.id;
+  fs.writeFileSync(botDataPath, JSON.stringify(data));
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("register")
+      .setLabel("S'enregistrer")
+      .setStyle(ButtonStyle.Primary)
+  );
   await channel.send({
     embeds: [
       {
